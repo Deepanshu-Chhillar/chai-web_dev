4744490f314cac4078653f4ad8e40916c2a4049b
Deepanshu Chillar <user.email : deepanshuchillar2005@gmail.com>
2026-06-16 18:11:33 +0530

    Uploading multiple files after a long time, from startup, clg-wrk & life

---
+++ chai-js-prt.2/colorChanger_Proj/chai.js
@@ -0,0 +1,20 @@
+let buttons = document.querySelectorAll(".button");
+let body = document.querySelector("body"); // Corrected querySelector to select the first element with class "body"
+buttons.forEach(function (button){
+console.log(button);
+button.addEventListener('click', function(e) {
+console.log(e)
+console.log(e.target)
+if(e.target.id === 'grey'){
+body.style.backgroundColor = e.target.id; }
+if(e.target.id === 'blue'){
+body.style.backgroundColor = e.target.id; }
+if(e.target.id === 'orange'){
+body.style.backgroundColor = e.target.id; }
+if(e.target.id === 'yellow'){
+body.style.backgroundColor = e.target.id; }
+
+});
+});
+
+
